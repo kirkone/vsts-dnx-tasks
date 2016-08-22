37388d504ce35e063486614d9fc5022fd1b481d9
Kirsten Kluge <live@kirkone.de>
2016-08-22 11:51:06 +0200

    Added Task
    Tast to generate Change Log file

---
+++ VSTS.DNX.Tasks.GenerateChangeLog/GenerateChangeLog.ps1
@@ -0,0 +1,47 @@
+﻿[CmdletBinding(DefaultParameterSetName = 'None')]
+param (
+    [String] [Parameter(Mandatory = $false)]
+    $NugetPath
+)
+
+Write-Verbose "Entering script ClearNugetCache.ps1"
+
+Write-Output "Parameter Values:"
+foreach($key in $PSBoundParameters.Keys)
+{
+    Write-Output ("    $key = $($PSBoundParameters[$key])")
+}
+
+$usedNugetPath = ""
+
+Write-Output "Deleting Cache..."
+
+if(!([string]::IsNullOrWhitespace("$NugetPath")) -and (Test-Path "$NugetPath"))
+{
+    $usedNugetPath = "$NugetPath"
+}
+elseif( !([string]::IsNullOrWhitespace("$env:NUGET_PACKAGES")) -and (Test-Path "$env:NUGET_PACKAGES") )
+{
+    $usedNugetPath = "$env:NUGET_PACKAGES"
+}
+elseif( !([string]::IsNullOrWhitespace("$env:USERPROFILE\.nuget\packages")) -and (Test-Path "$env:USERPROFILE\.nuget\packages") )
+{
+    $usedNugetPath = "$env:USERPROFILE\.nuget\packages"
+}
+else
+{
+    Write-Error "No NuGet Cache folder found!"
+    return
+}
+
+Write-Output "        Nuget cache folder:"
+Write-Output "            $usedNugetPath"
+Write-Output "        Items to remove: $(( Get-ChildItem "$usedNugetPath" ).Count)"
+
+
+Remove-Item -Recurse -Force "$usedNugetPath"
+mkdir $usedNugetPath | Out-Null
+
+Write-Output "    Done"
+
+Write-Verbose "Leaving script ClearNugetCache.ps1"
