f98b3e27641f58756fee382def7a069873a929da
kirkone <live@kirkone.de>
2017-02-01 01:13:10 +0100

    Bugfix for Add Virtual App Task
    - check if there is an folder for the added vApp
    - Redone the calls for the Azure PS

--- VSTS.DNX.Tasks.AddVirtualApp/AddVirtualApp.ps1
+++ VSTS.DNX.Tasks.AddVirtualApp/AddVirtualApp.ps1
@@ -17,6 +17,7 @@ param (
 
 Write-Verbose "Entering script AddVirtualApp.ps1"
 
+Write-Host " "
 Write-Host "Parameter Values:"
 foreach($key in $PSBoundParameters.Keys)
 {
@@ -24,9 +25,17 @@ foreach($key in $PSBoundParameters.Keys)
 }
 Write-Host " "
 
+Function JoinParts {
+    param ([string[]] $Parts, [string] $Separator = '/')
+
+    $search = '(?<!:)' + [regex]::Escape($Separator) + '+'
+    ($Parts | Where-Object {$_ -and $_.Trim().Length}) -join $Separator -replace $search, $Separator
+}
+
 Function Main
 {
-    $web = Get-AzureRmResource -ResourceGroupName SWMH-O365-Dashboard -ResourceType Microsoft.Web/sites/slots/config -ResourceName swmh-o365-dashboard/feature/web -ApiVersion 2015-08-01
+    $ResourceType = "Microsoft.Web/sites{0}" -f ("/slots","")[[string]::IsNullOrWhiteSpace($SlotName)]
+    $ResourceName = "$WebAppName{0}" -f ("/$SlotName","")[[string]::IsNullOrWhiteSpace($SlotName)]
 
     $newApp = @"
 {
@@ -37,16 +46,73 @@ Function Main
 }
 "@ | ConvertFrom-Json
 
+    Write-Host "Adding virtual application..."
+
+    $web = Get-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType $ResourceType/config -ResourceName $ResourceName/web -ApiVersion 2015-08-01
+
     if(-Not( $web.properties.virtualApplications.virtualPath -contains $newApp.virtualPath) )
     {
         $web.properties.virtualApplications += $newApp | select *
-        Set-AzureRmResource -PropertyObject $web.properties -ResourceGroupName SWMH-O365-Dashboard -ResourceType Microsoft.Web/sites/slots/config -ResourceName swmh-o365-dashboard/feature/web -ApiVersion 2015-08-01 -Force
-        Write-Host "Virtual application added."
+        Set-AzureRmResource -PropertyObject $web.properties -ResourceGroupName $ResourceGroupName -ResourceType $ResourceType/config -ResourceName $ResourceName/web -ApiVersion 2015-08-01 -Force
+        Write-Host "    Done."
     }
     else
     {
-        Write-Host "Virtual application allready exists."
+        Write-Host "    Virtual application allready exists."
     }
+
+    $publishingcredentials = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType $ResourceType/config -ResourceName $ResourceName/publishingcredentials -Action list -ApiVersion 2015-08-01 -Force
+
+    # Website not found -> cancel task with error!
+    if(!$publishingcredentials)
+    {
+        Write-Error "Publishing credentials not found! aborting...`n`r "
+        Write-Host "##vso[task.complete result=Failed;]Publishing credentials not found!"
+        exit 1
+    }
+
+    $username = $publishingcredentials.properties.publishingUserName
+    $password = $publishingcredentials.properties.publishingPassword
+    $base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+    $authHeader = @{Authorization=("Basic {0}" -f $base64Auth);"If-Match"="*"}
+
+    $slot = Get-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType $ResourceType -ResourceName $ResourceName -ApiVersion 2015-08-01
+
+    $baseUri = ($slot.properties.EnabledHostNames | Where-Object { $_ -like "*.scm.*"} | Select-Object -First 1)
+    $apiUserAgent = "powershell/1.0"
+
+    $vfsApiUri = JoinParts ("https://", $baseUri, "/api/vfs/site", $VirtualApplicationName, "/")
+
+    Write-Host " "
+    Write-Host "Checking folder for virtual application..."
+    try
+    {
+        Invoke-RestMethod -Uri $vfsApiUri -Headers $authHeader -UserAgent $userAgent -Method GET | Out-Null
+    }
+    catch
+    {
+        if($_.Exception.Response.StatusCode -eq "NotFound")
+        {
+            Write-Host "    Folder does not exist, creating..."
+            try
+            {
+                Invoke-RestMethod -Uri $vfsApiUri -Headers $authHeader -UserAgent $userAgent -Method PUT | Out-Null
+            }
+            catch
+            {
+                Write-Host ($_.Exception.Response | Format-List | Out-String)
+            }
+        }
+        else
+        {
+            Write-Host ($_.Exception.Response| Format-List | Out-String)
+            Write-Error "Can not create Folder $VirtualApplicationName! aborting...`n`r "
+            Write-Host "##vso[task.complete result=Failed;]Can not create Folder $VirtualApplicationName!"
+            exit 1
+        }
+    }
+    
+    Write-Host "    Done."
 }
 
 Main
